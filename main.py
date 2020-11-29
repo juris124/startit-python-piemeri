f41d2c28b4ccbb8df2758980a2ab6a56d6ed53dd
juris124 <31391698+juris124@users.noreply.github.com>
2020-11-29 14:45:23 +0200

    labojumi

--- main.py
+++ main.py
@@ -3,20 +3,31 @@ from pprint import pprint
 import re
 
  
-html = apstrada_datni("dati/piemers.html")
+# html = apstrada_datni("dati/piemers.html")
 
-pprint(html.head.title.text)
-pprint(html.body.h2)
-pprint(html.body.p)
-pprint(html.find_all('p'))
-pprint(html.find_all(id=True))
-pprint(html.find_all(id="viens"))
-pprint(html.find_all("p", class_="teksts"))
+# pprint(html.head.title.text)
+# pprint(html.body.h2)
+# pprint(html.body.p)
+# pprint(html.find_all('p'))
+# pprint(html.find_all(id=True))
+# pprint(html.find_all(id="viens"))
+# pprint(html.find_all("p", class_="teksts"))
 
 # html = apstrada_lapu("https://www.tvnet.lv")
 # pprint(html.head.title.text)
-# # raksti = html.find_all('a', class_="list-article__url")
-# # print(len(raksti))
-# raksti = html.find_all('a', class_="list-article__url", string=re.compile("Latv"))
+# raksti = html.find_all('a', class_="list-article__url")
+# print(len(raksti))
+# print(raksti[0])
+#raksti = html.find_all('a', class_="list-article__url", string=re.compile("Latv")) # izvada tikai kur ir vārds `Latv`
+#raksti = html.find_all('a', class_="list-article__url") # izvada visus ierakstus
+# raksti = html.find_all('a', class_="list-article__url", string=re.compile("Covid-19"))
 # for r in raksti:
 #     print(r.text)
+
+# idejas - ss.lv skripts automašīnu atlasai
+#   vai arī dzīvokļa iegādei
+#   interneta veikala cenas var salīdzināt precēm
+
+# patstāvīgais darbs
+html = apstrada_lapu("https://preili.lv/pasvaldiba/kontaktinformacija/strukturu-iestazu-darbinieku-kontakti/")
+pprint(html.head.title.text)
